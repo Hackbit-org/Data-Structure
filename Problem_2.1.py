a5da76b0d9709b2a9a42aa3cb31b9edc171362aa
Sakil Hasan Saikat <72136146+0xSaikat@users.noreply.github.com>
2024-04-24 12:07:09 +0600

    Create Problem_2.1.py

---
+++ Problem_2.1.py
@@ -0,0 +1,12 @@
+# Problem 2.1 - 
+
+a = []
+
+for i in range(5):
+    uIn = input("Enter 5 number : ")
+    a.append(uIn)
+
+print("You inter thouse numbers " + str(a))
+
+la_num = max(a)
+print("The largest number from your input is : " + str(la_num))
